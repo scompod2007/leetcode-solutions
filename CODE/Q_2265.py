72e1c3a86ecdee408e7d95e38e737b076fcd1f91
scompod2007 <scompod2007@gmail.com>
2026-09-10 21:35:48 +0530

    Completed Ques.No 2265

---
+++ CODE/Q_2265.py
@@ -0,0 +1,22 @@
+class Solution:
+    def averageOfSubtree(self, root: TreeNode) -> int:
+        
+        def dfs(node: TreeNode) -> tuple:
+            if not node:
+                return 0, 0
+            
+            left_sum, left_count = dfs(node.left)
+            right_sum, right_count = dfs(node.right)
+            
+            subtree_sum = left_sum + right_sum + node.val
+            subtree_count = left_count + right_count + 1
+            
+            if subtree_sum // subtree_count == node.val:
+                nonlocal result
+                result += 1
+            
+            return subtree_sum, subtree_count
+        
+        result = 0
+        dfs(root)
+        return result

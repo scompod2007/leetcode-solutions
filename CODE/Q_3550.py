41b72138091e7a3d528d08ac3441012ab3d9527c
scompod2007 <scompod2007@gmail.com>
2026-09-24 14:11:21 +0530

    Completed Ques.No 3550

---
+++ CODE/Q_3550.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    def smallestIndex(self, nums: List[int]) -> int:
+
+        for i in  range(len(nums)):
+            if sum(map(int, str(nums[i]))) == i:
+                return i
+
+        return - 1
